f4692c014aa4a84e73a0c676478432b05c1a53f6
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-07-09 16:19:27 +0200

    When containers get 3 colors, score inscrease 5 and ctn reset

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./app.css";
 
 function App() {
-  const  [widgets, setWidgets] = useState([]);
+  const [widgets, setWidgets] = useState([]);
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    if(widgets.length >= 3){
+      setScore(score + 5);
+      setWidgets([]);
+    }
+  }, [widgets, score]);
 
   function handleOnDrag(e, widgetType) {
-    e.dataTransfer.setData("widgetType", widgetType );
+    e.dataTransfer.setData("widgetType", widgetType);
   }
-  
+
   function handleOnDrop(e) {
     const widgetType = e.dataTransfer.getData("widgetType");
     console.log("widgetType", widgetType);
@@ -18,43 +26,41 @@ function App() {
     e.preventDefault();
   }
 
-
   return (
     <div className="App">
       <div className="widgets">
-        <div 
-        className="widget"
-        draggable
-        onDragStart={(e) => handleOnDrag(e, "Widget A")}
+        <div
+          className="widget"
+          draggable
+          onDragStart={(e) => handleOnDrag(e, "Widget A")}
         >
           Widget A
         </div>
-        <div 
-        className="widget"
-        draggable
-        onDragStart={(e) => handleOnDrag(e, "Widget B")}
+        <div
+          className="widget"
+          draggable
+          onDragStart={(e) => handleOnDrag(e, "Widget B")}
         >
           Widget B
         </div>
-        <div 
-        className="widget"
-        draggable
-        onDragStart={(e) => handleOnDrag(e, "Widget C")}
+        <div
+          className="widget"
+          draggable
+          onDragStart={(e) => handleOnDrag(e, "Widget C")}
         >
           Widget C
         </div>
       </div>
-      <div className="page" onDrop={handleOnDrop} onDragOver={handleDragOver} >
+      <div className="page" onDrop={handleOnDrop} onDragOver={handleDragOver}>
         {widgets.map((widget, index) => (
-            <div className="dropped-widget" key={index} >
-              {widget}
-            </div>
-
+          <div className="dropped-widget" key={index}>
+            {widget}
+          </div>
         ))}
       </div>
-
+      <div className="score">{score}</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
